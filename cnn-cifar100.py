e740f1fe2e306e153bb2cb62af2ae40fa085f7e4
Sourabh <sr925041@dal.ca>
2023-08-24 16:28:01 -0300

    vit_scratch

---
+++ cnn-cifar100.py
@@ -0,0 +1,134 @@
+from keras.datasets import cifar100
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import MaxPooling2D, Conv2D, Flatten, Dense, Activation, Dropout
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPool2D
+from keras.layers.core import Dense,Activation,Dropout,Flatten
+import time
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.initializers import RandomNormal, Constant
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import optimizers
+
+
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+# shape of array
+print(f"x_train shape: {x_train.shape}")
+print(f"x_train shape: {y_train.shape}")
+print(f"x_train shape: {x_test.shape}")
+print(f"x_train shape: {y_test.shape}")
+
+# number of samples in dataset
+print(f"Train: {x_train.shape[0]}")
+print(f"Test: {x_train.shape[0]}")
+
+# show random 16 images from cifar100
+plt.figure(figsize=(10,10))
+for i in range(16):
+  rand_num = np.random.randint(0, 50000)
+  cifar_img = plt.subplot(4,4,i+1)
+  plt.imshow(x_train[rand_num])
+
+#normalize
+train_images = x_train.astype('float32') / 255
+test_images = x_test.astype('float32') / 255
+
+#transform labels to one hot encoding
+train_labels = to_categorical(y_train)
+test_labels = to_categorical(y_test)
+
+def training_plot(history):
+  acc = history.history['acc']
+  val_acc = history.history['val_acc']
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+  epochs = range(1, len(acc)+1)
+  plt.plot(epochs, acc, 'bo', label='Training acc')
+  plt.plot(epochs, val_acc, 'b', label='Validation acc')
+  plt.title('Training and validation accuracy')
+  plt.legend()
+  plt.figure()
+  plt.plot(epochs, loss, 'bo', label='Training loss')
+  plt.plot(epochs, val_loss, 'b', label='Validation loss')
+  plt.title('Training and validation loss')
+  plt.legend()
+  plt.show()
+
+
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), padding='same', input_shape=(32, 32, 3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(256, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(512, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(512, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(512, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(512, (3, 3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+model.add(Dense(1024))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization(momentum=0.95,
+                             epsilon=0.005,
+                             beta_initializer=RandomNormal(mean=0.0, stddev=0.05),
+                             gamma_initializer=Constant(value=0.9)))
+model.add(Dense(100, activation='softmax'))
+model.summary()
+
+train_datagen = ImageDataGenerator(
+    rotation_range=20,
+    horizontal_flip=True,
+)
+
+X_train, X_validation, y_train, y_validation = train_test_split(train_images, train_labels, test_size=0.2, random_state=93)
+train_datagen.fit(X_train)
+
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(learning_rate=1e-4),
+              metrics=['acc'])
+
+training_start = time.time()
+history = model.fit(train_datagen.flow(X_train, y_train, batch_size=64),
+          steps_per_epoch=100,
+          epochs=100,
+          validation_data=(X_validation, y_validation),
+          verbose=1)
+training_stop = time.time()
+training_time = training_stop - training_start
+print(f"Training time: {training_time}")
